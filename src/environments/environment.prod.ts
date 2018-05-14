b3723741581fe6f8c7a793576becc2bfe2c47cee
simonwalenkamp <sinnak.sinnak@hotmail.com>
2018-05-14 11:50:54 +0200

    Routing

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyCB-sFHjl7L9zcUTC8-FM4U-miZSen5ZO4',
+    authDomain: 'wishme-a73d1.firebaseapp.com',
+    databaseURL: 'https://wishme-a73d1.firebaseio.com',
+    projectId: 'wishme-a73d1',
+    storageBucket: 'wishme-a73d1.appspot.com',
+    messagingSenderId: '391332383230'
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyCB-sFHjl7L9zcUTC8-FM4U-miZSen5ZO4',
+    authDomain: 'wishme-a73d1.firebaseapp.com',
+    databaseURL: 'https://wishme-a73d1.firebaseio.com',
+    projectId: 'wishme-a73d1',
+    storageBucket: 'wishme-a73d1.appspot.com',
+    messagingSenderId: '391332383230'
+  }
 };
