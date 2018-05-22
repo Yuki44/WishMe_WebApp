85fae36c6dceb52f64b98835a11c5428578bff64
simonwalenkamp <sinnak.sinnak@hotmail.com>
2018-05-22 13:32:54 +0200

    Revert "Revert "Revert "Merge pull request #32 from Yuki44/Michal"""
    This reverts commit 6d5c5220901ed1ca5844f162170f2e14caf095fe.

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,7 +1,3 @@
-import { ProfileModule } from './../profile/profile.module';
-import { AddProfileComponent } from './../profile/add-profile/add-profile.component';
-import { LoggedInService } from './shared/logged-in.service';
-import { AuthGuardService } from './shared/auth-guard.service';
 import { AuthService } from './shared/auth.service';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FlexLayoutModule } from '@angular/flex-layout';
@@ -9,8 +5,7 @@ import { MatCardModule } from '@angular/material/card';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login/login.component';
-import { MatButtonModule, MatFormFieldModule, MatInputModule, MatSnackBarModule,
-  MatIconModule, MatSpinner, MatProgressSpinnerModule } from '@angular/material';
+import { MatButtonModule, MatFormFieldModule, MatInputModule, MatSnackBarModule } from '@angular/material';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AngularFireAuth } from 'angularfire2/auth';
 import { SignupComponent } from './signup/signup.component';
@@ -26,19 +21,10 @@ import { SignupComponent } from './signup/signup.component';
     BrowserAnimationsModule,
     ReactiveFormsModule,
     FormsModule,
-    MatSnackBarModule,
-    ProfileModule,
-    MatFormFieldModule,
-    MatInputModule,
-    MatButtonModule,
-    MatCardModule,
-    MatSnackBarModule,
-    MatIconModule,
-    MatProgressSpinnerModule,
-    BrowserAnimationsModule
+    MatSnackBarModule
   ],
   declarations: [LoginComponent, SignupComponent],
   exports: [ MatButtonModule, MatCardModule, FlexLayoutModule ],
-  providers: [ AuthService, AuthGuardService, LoggedInService, AddProfileComponent ]
+  providers: [ AuthService]
 })
 export class AuthModule { }
