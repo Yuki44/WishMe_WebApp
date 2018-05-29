105d4d9fbb7206239f3e4bd26a903dbb1d23ee8d
Carlos <carl7755@easv365.dk>
2018-05-29 10:19:09 +0200

    some comments

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,23 +1,25 @@
 'use strict';
 const functions = require('firebase-functions');
-const admin = require('firebase-admin');
+const admin = require('firebase-admin'); // Admin is necessary in order to call firebase CRUD methods
 admin.initializeApp(functions.config().firebase);
 
-exports.deletedWishlist = functions.firestore
-  .document('wishlist/{id}')
-  .onDelete((snap, context) => {
+exports.deletedWishlist = functions.firestore // exports the function to Firebase
+  .document('wishlist/{id}') // We check for the wishlist id
+  .onDelete((snap, context) => { // When a wishlist is deleted
+
+    // Block of code to run:
 
        admin.firestore().collection('wish').where('owner', '==', snap.id).get().then(refs => {
-         refs.forEach(snapshot => {
+         refs.forEach(snapshot => { // We check for the wish with the owner id from deleted wishlist
            console.log("snapshot: ", JSON.stringify(snapshot));
-           snapshot.ref.delete().then(function () {
+           snapshot.ref.delete().then(function () { // Then we get the snapshot of it so we can delete it
              console.log("1 documents successfully deleted!")
            }) .catch(function (error) {
              console.error("Error removing document: ", error);
            });
          })
        });
-    return true;
+    return true; // Cloud Functions needs to return something or especially a promise
   });
 
 
